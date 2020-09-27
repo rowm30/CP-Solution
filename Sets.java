5209108001c2f455c1af2367d8f7357d110b871d
KMV <rowm30@gmail.com>
2020-09-27 20:11:25 +0530

    Three Solutions2

--- Sets.java
+++ Sets.java
@@ -1,12 +1,21 @@
 import java.util.*;
 
 public class Sets {
+    public static int HCF(int[] arr){
+        return 0;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int[] arr1 = new int[a];
         int[] arr2 = new int[b];
-        
+        for (int i = 0; i < a; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        for (int i = 0; i < b; i++) {
+            arr2[i] = sc.nextInt();
+        }
     }
+
 }
