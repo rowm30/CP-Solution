a9eccdd287cc507846ff3943dcc92f7828b5bb40
KMV <rowm30@gmail.com>
2020-09-28 01:29:38 +0530

    Three Solutions3

--- Sets.java
+++ Sets.java
@@ -15,19 +15,38 @@ public class Sets {
         return solarr;
     }
     
+    public static int[] CE(int[] parr1,int[] parr2){
+        int[] sol = new int[parr1.length];
+        for (int i = 0; i < parr1.length; i++) {
+            if (parr1[i]==parr2[i]) {
+                sol[i] = parr1[i];
+            }
+        }
+        return sol;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int[] arr1 = new int[a];
         int[] arr2 = new int[b];
+        int[] fina = arr2;  
+        int ans = 0;
         for (int i = 0; i < a; i++) {
             arr1[i] = sc.nextInt();
         }
         for (int i = 0; i < b; i++) {
             arr2[i] = sc.nextInt();
         }
-        
+        for (int i = 0; i < arr2.length; i++) {
+            int[] temp1 = CF(arr2[i]);
+            fina = CE(fina,temp1);
+        }
+
+        for (int i = 0; i < fina.length; i++) {
+            System.out.println(fina[i]);
+        }
     }
 
 }
