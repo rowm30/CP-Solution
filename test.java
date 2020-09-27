9470cdb6989769428f4d67234416fde3e713d442
KMV <rowm30@gmail.com>
2020-09-27 21:54:10 +0530

    Three Solutions3

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Sets {
+public class test {
     public static int[] CF(int num){
         Deque<Integer> dq = new LinkedList<Integer>();
         for (int i = 2; i < num; i++) {
@@ -15,19 +15,12 @@ public class Sets {
         return solarr;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        int[] arr1 = new int[a];
-        int[] arr2 = new int[b];
-        for (int i = 0; i < a; i++) {
-            arr1[i] = sc.nextInt();
+        int[] b = CF(a);
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
         }
-        for (int i = 0; i < b; i++) {
-            arr2[i] = sc.nextInt();
-        }
-        
     }
-
 }
