cc96ee1a942f10a6546d89c39e6661ac10db9e56
KMV <rowm30@gmail.com>
2020-10-09 16:10:52 +0530

    mayank

--- countingValleys.java
+++ countingValleys.java
@@ -4,17 +4,18 @@ public class countingValleys {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int steps = sc.nextInt();
-        String[] arr = new String[steps]; 
+        String s = sc.next();
+        char[] arr = s.toCharArray();
         int level = 0;
         int ans = 0;
-        for (int i = 0; i < steps; i++) {
+        for (int i = 0; i < (steps-1); i++) {
             if (arr[i] == 'U') {
                 level++;
             } else {
                 level--;
             }
             if (level == -1) {
-                if (arr[i+1 == 'U']) {
+                if (arr[i+1] == 'U') {
                     ans++;
                 }
             }
